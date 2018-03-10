807c1f22930d92071c2ff7c2e954a0eb73a0ad06
Diego <ligongxue0628@gmail.com>
2018-03-10 05:33:13 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,4 +14,30 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request -> name,
+            'email' => $request -> email,
+            'password' => bcrypt($request -> password),
+        ]);
+
+
+        session() -> flash ('success', 'Hello, your application has been submitted.');
+
+        return redirect() -> route('users.show', [$user]);
+    }
+
 }
